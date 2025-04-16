209c695eadf0e3942325f9995bfde94bc125225d
xhiew <hieunx1@vnpay.vn>
2025-04-16 22:47:52 +0700

    add extension, constant

---
+++ SwiftArena/SwiftArena/Utilities/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  SwiftArena
+//
+//  Created by xhiew on 16/4/25.
+//
+
+import Foundation
+
+struct Constant {
+    
+}
---
+++ SwiftArena/SwiftArena/Utilities/String+Ext.swift
@@ -0,0 +1,12 @@
+//
+//  String+Ext.swift
+//  SwiftArena
+//
+//  Created by xhiew on 16/4/25.
+//
+
+import Foundation
+
+extension String {
+    static let empty = ""
+}
